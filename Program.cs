bc8075fc8ba235bed2f350181e1e39443201c265
Paweł Stolarski <stolarski.paw@gmail.com>
2019-07-01 13:14:52 +0200

    + basic pinger functionality

--- Program.cs
+++ Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using System.Net.NetworkInformation;
+using System.Threading;
 
-namespace Pinger
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+namespace App {
+  class Program {
+    static void Main(string[] args) {
+      Timer timer = new Timer( t => _Ping(), null, 1, 1000 );
+
+      Console.ReadKey();
     }
+
+    static void _Ping() {
+      Ping ping = new Ping();
+      PingReply pingReply = ping.Send( "google.com" );
+
+      Console.WriteLine();
+      Console.WriteLine( "Address: " + pingReply.Address );
+      Console.WriteLine( "Roundtrip time: " + pingReply.RoundtripTime );
+      Console.WriteLine( "TTL (Time To Live): " + pingReply.Options.Ttl );
+    }
+  }
 }
