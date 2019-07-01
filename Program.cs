c3342409cb1534711c89a53f8f823756535f4e37
Paweł Stolarski <stolarski.paw@gmail.com>
2019-07-01 14:52:21 +0200

    Replaced text data with chart

--- Program.cs
+++ Program.cs
@@ -1,23 +1,56 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net.NetworkInformation;
 using System.Threading;
 
 namespace App {
-  class Program {
-    static void Main(string[] args) {
-      Timer timer = new Timer( t => _Ping(), null, 1, 1000 );
+  class Pinger {
+    static List<long> _jumps = new List<long>();
 
+    static void Main(string[] args) {
+      Timer timer = new Timer( t => Ping(), null, 1, 1000 );
       Console.ReadKey();
     }
 
-    static void _Ping() {
+    static void Ping() {
       Ping ping = new Ping();
       PingReply pingReply = ping.Send( "google.com" );
+      int different = 3;
+      int x = Console.WindowWidth - different; // 25;
+      int y = Console.WindowHeight - different; // 5;
 
-      Console.WriteLine();
-      Console.WriteLine( "Address: " + pingReply.Address );
-      Console.WriteLine( "Roundtrip time: " + pingReply.RoundtripTime );
-      Console.WriteLine( "TTL (Time To Live): " + pingReply.Options.Ttl );
+      bool [,] console = new bool[ y, x ];
+      long highestJump = y * 3;
+      float multiplier;
+
+      Console.Clear();
+
+      _jumps.Insert( 0, pingReply.RoundtripTime );
+      _jumps.ForEach( jump => {
+        if ( jump > highestJump )
+          highestJump = jump;
+      } );
+
+      multiplier = (float) y / (float) highestJump;
+
+      for ( int i = 0;  i < y;  ++i )
+        for ( int j = 0;  j < x;  ++j ) {
+          bool cell;
+
+          if ( j < _jumps.Count )
+            cell = _jumps[ j ] * multiplier >= i;
+          else
+            cell = false;
+
+          console[ i, j ] = cell;
+        }
+
+      for ( int i = y - 1;  i >= 0;  --i ) {
+        Console.Write( "\n" );
+
+        for ( int j = 0;  j < x;  ++j )
+          Console.Write( console[ i, j ]  ?  "#"  :  " " );
+      }
     }
   }
 }
